b9dd96028cc5bd848b4c0c7b37593438b4596074
tibettiger <40263709+tibettiger@users.noreply.github.com>
2022-08-31 16:27:11 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='KBPRNA',
+    version='0.0.1',
+    packages=['KBPRNA'],
+    desription='Machine Learning model of predicting kinase activity',
+    license='MIT',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'scanpy',
+        'anndata',
+        'matplotlib',
+        'xgboost',
+        'sklearn',
+        'random'
+    ]
+)
